a53670c0d9f45adf6234d1cb2273a39079c71048
samuelgigs <samuelleads01@gmail.com>
2022-10-13 12:56:11 -0700

    C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* This print what programming is like
+* return a value of 0
+*/
+
+int main (void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
