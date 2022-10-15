deb24a69ad7a06ec87b865d10c3814cc6041777f
samuelgigs <samuelleads01@gmail.com>
2022-10-15 00:46:24 -0700

    program will assign a random number to the variable

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 Success
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Enter number n:\n");
+	scanf("%d", &n);
+	if (n > 0)
+	{
+	printf("is positive\n");
+	}
+	else if (n == 0)
+	{
+	printf("is Zero\n");
+	}
+	else if (n < 0)
+	{
+	printf("is negative\n");
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Project containing conditional statements
