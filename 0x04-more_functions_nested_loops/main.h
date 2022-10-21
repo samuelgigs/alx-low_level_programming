c7b8ee26948d0e53d4378be8dcc8a93f877dfa16
samuelgigs <samuelleads01@gmail.com>
2022-10-21 12:12:12 -0700

    main file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,9 @@
 #ifndef MAIN_
 #define MAIN_
 
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-int _putchar(char c);
 void print_numbers(void);
 #endif
