5011431d808ec624e60ff2091d5c0196666e7e31
samuelgigs <samuelleads01@gmail.com>
2022-10-21 03:22:55 -0700

    function that checks for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,12 @@
 /**
 * _isupper - Entry point
 * @c: first integer
-* Return: 0 Succes
+* Return: 0 or 1
 */
 
 int _isupper(int c)
 {
-	if ( c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
