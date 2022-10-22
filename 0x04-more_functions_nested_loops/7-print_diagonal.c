853d6fa1a37cced5bbd3d42aaee8f0f14ebd6202
samuelgigs <samuelleads01@gmail.com>
2022-10-22 02:47:29 -0700

    print element in diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,21 +14,15 @@ void print_diagonal(int n)
 	{
 		_putchar('\n');
 	}
-	else{
+
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j < i; j++)
 		{
-			if (j == i)
-			{
-			_putchar('\\');
-			}
-			else
-			{
 			_putchar(' ');
-			}
 		}
+	_putchar('\\');
 	_putchar('\n');
 	}
-	}
+
 }
