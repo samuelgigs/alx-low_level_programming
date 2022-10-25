bd893f2688d29bf01e05b7800bd64919a503caf6
samuelgigs <samuelleads01@gmail.com>
2022-10-25 11:41:07 -0700

    function that copies the string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_strcpy - Entry point
+*@dest: destination
+*@src: source
+*Return: Dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+
+	while (*(src + len) != '\0')
+	{
+		*(dest + len) = *(src + len);
+		len++;
+	}
+	*(dest + len) = '\0';
+	return (dest);
+}
