9cdd219ee3b3d0d68697e11fac304922b3ef6f68
samuelgigs <samuelleads01@gmail.com>
2022-10-25 10:59:43 -0700

    prints every other character of a string, starting with the first character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*puts2 - Entry point
+*@str: first input
+*
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+	_putchar('\n');
+
+}
