70feedf3931d69843dec4b56e304ae57780d150c
samuelgigs <samuelleads01@gmail.com>
2022-10-27 11:19:35 -0700

    string congatination

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*_strcat - Entry point
+* @dest: first integer
+* @src: secong integer
+* Return: return character
+*/
+
+char *_strcat(char *dest, char *src)
+{
+
+	int len = 0, i;
+
+	while (dest[len])
+	{
+		len++;
+	}
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
